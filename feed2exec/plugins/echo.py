f5e3ad36a893ce4a07e7b6c4edcf9499cd303750
Antoine Beaupré <anarcat@debian.org>
2017-09-03 21:30:41 -0400

    merge echo/test plugins

--- feed2exec/plugins/echo.py
+++ feed2exec/plugins/echo.py
@@ -1,11 +1,15 @@
-"""Echo plugin
+"""Test plugin
 ===========
 
-The echo plugin simply outputs, to standard output, the arguments it
-receives. It can be useful to test your configuration.
+This plugin outputs, to standard output, the arguments it receives. It
+can be useful to test your configuration. It also creates a side
+effect for the test suite to determine if the plugin was called.
 """
 
 
 class Output(object):
+    called = None
+
     def __init__(self, *args, **kwargs):
         print("arguments received: %s, kwargs: %s" % (args, kwargs))
+        Output.called = args
--- feed2exec/plugins/test.py
+++
@@ -1,5 +0,0 @@
-class Output(object):
-    called = None
-
-    def __init__(self, *args):
-        Output.called = args
