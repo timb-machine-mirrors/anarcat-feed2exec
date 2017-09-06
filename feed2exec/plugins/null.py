de28be228a803eb2e29de97d42e4780fe949b113
Antoine Beaupré <anarcat@debian.org>
2017-09-06 18:37:57 -0400

    simplify and document the null plugin

--- feed2exec/plugins/null.py
+++ feed2exec/plugins/null.py
@@ -1,4 +1,10 @@
-class Output(object):
-    def __init__(self, *args, **kwargs):
-        pass
+"""Null plugin
+===========
 
+This plugin does nothing. It can be useful in cases where you want to
+catchup with imported feeds.
+"""
+
+
+def output(*args, **kwargs):
+    pass
