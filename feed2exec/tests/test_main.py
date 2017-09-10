8039fb77aadae8b640845f60520ade235cf1994b
Antoine Beaupré <anarcat@debian.org>
2017-09-10 06:21:09 -0400

    add failing test for fmarier

--- feed2exec/tests/test_main.py
+++ feed2exec/tests/test_main.py
@@ -4,13 +4,15 @@
 from __future__ import division, absolute_import
 from __future__ import print_function
 
-
+import html
 import json
 
-
 from click.testing import CliRunner
+import html2text
+
 from feed2exec.__main__ import main
-from feed2exec.tests.test_feeds import ConfFeedStorage, test_data, test_nasa
+from feed2exec.tests.test_feeds import (ConfFeedStorage, test_data,
+                                        test_nasa, find_test_file)
 
 
 def test_usage():
@@ -53,5 +55,38 @@ def test_basics(tmpdir_factory):
     assert conf_dir.join('feed2exec.ini').check()
     assert result.exit_code == 0
 
+    maildir = conf_dir.join('maildir')
+    test_path = find_test_file('planet-debian.xml')
+    result = runner.invoke(main, ['--config', str(conf_dir),
+                                  'add', 'planet-debian',
+                                  'file://' + test_path,
+                                  '--output', 'feed2exec.plugins.maildir',
+                                  '--output_args',
+                                  str(maildir) + ' to@example.com',
+                                  '--filter', 'feed2exec.plugins.html2text'])
     result = runner.invoke(main, ['--config', str(conf_dir),
                                   'fetch'])
+    assert result.exit_code == 0
+    assert maildir.check()
+    for path in maildir.join('planet-debian').join('new').visit():
+        body = path.read()
+        if 'Marier' in body:
+            break
+    else:
+        assert False, "entry from Francois Marier not found"
+
+    with open(test_path) as xml:
+        data = "".join(xml.readlines()[61:143])
+        data = data.replace("     <description>", '')
+        data = data.replace("</description>", '')
+    assert '''To: to@example.com
+From: planet-debian <to@example.com>
+Subject: =?utf-8?q?Fran=C3=A7ois_Marier=3A_TLS_Authentication_on_Freenode_and_OFTC?=
+Date: Sat, 09 Sep 2017 04:52:47 -0000
+Content-Transfer-Encoding: quoted-printable
+MIME-Version: 1.0
+Content-Type: text/plain; charset="utf-8"
+
+http://feeding.cloud.geek.nz/posts/tls_authentication_freenode_and_oftc/
+
+%s''' % html2text.html2text(html.unescape(data)) == body
