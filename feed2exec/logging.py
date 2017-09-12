e238c68196968e5dec37426355c4a29195bb0798
Antoine Beaupré <anarcat@debian.org>
2017-09-12 18:31:47 -0400

    remove unused advancedLogger code

--- feed2exec/logging.py
+++ feed2exec/logging.py
@@ -14,11 +14,7 @@
 
 from __future__ import absolute_import
 
-import getpass
 import logging
-from logging.handlers import SMTPHandler, MemoryHandler
-import socket
-import time
 import warnings
 
 
@@ -45,7 +41,6 @@ def _log_warning(message, category, filename, lineno, file=None, line=None):
 
 
 def advancedConfig(level='warning', stream=None, syslog=False, prog=None,
-                   email=False, smtpparams=None,
                    logfile=None, logFormat='%(levelname)s: %(message)s',
                    **kwargs):
     '''setup standard Python logging facilities
@@ -103,106 +98,3 @@ def advancedConfig(level='warning', stream=None, syslog=False, prog=None,
         handler = logging.FileHandler(logfile)
         handler.setFormatter(logging.Formatter(logFormat))
         logger.addHandler(handler)
-    if email:
-        subject = smtpparams.get('subject', '')
-        fromaddr = smtpparams.get('fromaddr', None)
-        smtpserver = smtpparams.get('mailhost', None)
-        smtpuser = smtpparams.get('user', None)
-        smtppass = smtpparams.get('pass', None)
-        # XXX: need to do MX discovery
-        if not smtpserver:
-            _, smtpserver = email.split('@', 1)
-        if not fromaddr:
-            fromaddr = getpass.getuser() + '@' + socket.getfqdn()
-        credentials = None
-        if smtpuser:
-            if not smtppass:
-                smtppass = getpass.getpass('enter SMTP password for %s: '
-                                           % smtpserver)
-            credentials = (smtpuser, smtppass)
-        handler = BufferedSMTPHandler(smtpserver,
-                                      fromaddr,
-                                      email,
-                                      subject,
-                                      secure=(),
-                                      credentials=credentials,
-                                      flushLevel=logging.CRITICAL)
-        handler.setFormatter(logging.Formatter(logFormat))
-        logger.addHandler(handler)
-
-
-class BufferedSMTPHandler(SMTPHandler, MemoryHandler):
-    """A handler class which sends records only when the buffer reaches
-    capacity. The object is constructed with the arguments from
-    SMTPHandler and MemoryHandler and basically behaves as a merge
-    between the two classes.
-
-    The SMTPHandler.emit() implementation was copy-pasted here because
-    it is not flexible enough to be overridden. We could possibly
-    override the format() function to instead look at the internal
-    buffer, but that would have possibly undesirable side-effects.
-    """
-
-    def __init__(self, mailhost, fromaddr, toaddrs, subject,
-                 credentials=None, secure=None,
-                 capacity=5000, flushLevel=logging.ERROR, retries=1):
-        SMTPHandler.__init__(self, mailhost, fromaddr, toaddrs, subject,
-                             credentials=None, secure=None)
-        self.retries = retries
-        MemoryHandler.__init__(self, capacity=capacity, flushLevel=flushLevel)
-
-    def emit(self, record):
-        '''buffer the record in the MemoryHandler'''
-        MemoryHandler.emit(self, record)
-
-    def flush(self):
-        """Flush all records.
-
-        Format the records and send it to the specified addressees.
-
-        The only change from SMTPHandler here is the way the email
-        body is created.
-
-        """
-        if self.retries < 0:
-            logging.error('Could not send email: %s', self.lastException)
-        if len(self.buffer) <= 0:
-            return
-        body = ''
-        for record in self.buffer:
-            body += self.format(record) + "\n"
-        try:
-            import smtplib
-            from email.utils import formatdate
-            port = self.mailport
-            if not port:
-                port = smtplib.SMTP_PORT
-            smtp = smtplib.SMTP(self.mailhost, port, timeout=self._timeout)
-            msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\nDate: %s\r\n\r\n%s" % (
-                            self.fromaddr,
-                            ",".join(self.toaddrs),
-                            self.getSubject(record),
-                            formatdate(), body)
-            if self.secure is not None:
-                smtp.ehlo()
-                smtp.starttls(*self.secure)
-                smtp.ehlo()
-            if self.username:
-                smtp.login(self.username, self.password)
-            smtp.sendmail(self.fromaddr, self.toaddrs, msg)
-            smtp.quit()
-            logging.info('sent email to %s using %s',
-                         self.toaddrs, self.mailhost)
-            self.buffer = []
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except smtplib.SMTPRecipientsRefused as e:
-            for email, error in e.recipients.iteritems():
-                if error[0] == 450:  # greylisting
-                    logging.info('temporary error, waiting 5 minutes to send')
-                    self.retries -= 1
-                    time.sleep(5*60)
-                    self.lastException = e
-                    self.flush()
-        except:
-            self.handleError(record)
