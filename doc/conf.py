d7bbf42460652bf1e0e7d26d4c8ef9638425be12
Antoine Beaupré <anarcat@debian.org>
2017-09-21 12:35:16 -0400

    use the right manpage

--- doc/conf.py
+++ doc/conf.py
@@ -86,7 +86,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, mod.__prog__, u'%s Documentation' % mod.__prog__,
+    ('usage', mod.__prog__, u'%s Documentation' % mod.__prog__,
      [author], 1)
 ]
 
