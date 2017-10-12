a8748c477ebafe3b09822d029277ed8ca3147e99
Antoine Beaupré <anarcat@debian.org>
2017-10-12 09:56:00 -0400

    cleanup the wayback code a little more

--- feed2exec/plugins/wayback.py
+++ feed2exec/plugins/wayback.py
@@ -3,12 +3,19 @@ import logging
 import requests
 
 
+WAYBACK_URL = 'https://web.archive.org'
+
+
 def output(*args, item=None, **kwargs):
     if item and item.get('link'):
-        res = requests.head('https://web.archive.org/save/%s'
-                            % item.get('link'))
+        res = requests.head('%s/save/%s' % (WAYBACK_URL, item.get('link')))
         res.headers['status_code'] = res.status_code
+        archive_location = WAYBACK_URL + res.headers['Content-Location']
         if res.status_code == requests.codes.ok:
-            logging.info('saved to wayback machine (status: %(status_code)s, URL: %(Content-Location)s, cache: %(X-Page-Cache)s)', res.headers)  # noqa
+            logging.info('URL %s saved to wayback machine: %s',
+                         item.get('link'), archive_location, extra=res)
+            return True
         else:
-            logging.warn('wayback machine failed (status: %(status_code)s, URL: %(Content-Location)s)', res.headers)  # noqa
+            logging.warn('wayback machine failed to save URL %s, status %d',
+                         item.get('link'), res.status_code, extra=res)  # noqa
+            return False
