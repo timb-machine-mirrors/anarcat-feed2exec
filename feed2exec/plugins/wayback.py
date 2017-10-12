242fac901e72482a5fd1f9f32343c40b4f53390a
Antoine Beaupré <anarcat@debian.org>
2017-10-11 21:14:47 -0400

    fix horrible syntax errors in wayback plugin

--- feed2exec/plugins/wayback.py
+++ feed2exec/plugins/wayback.py
@@ -1,12 +1,14 @@
+import logging
+
 import requests
+
 
 def output(*args, item=None, **kwargs):
     if item and item.get('link'):
-        res = requests.head('https://web.archive.org/save/%s' % item.get('link'))
-	res.headers['status_code'] = r.status_code
-	if r.status_code == requests.codes.ok:
-            logging.info('saved to wayback machine (status: %(status_code)s, URL: %(Content-Location)s, cache: %(X-Page-Cache)s)',
-                         res.headers))
+        res = requests.head('https://web.archive.org/save/%s'
+                            % item.get('link'))
+        res.headers['status_code'] = res.status_code
+        if res.status_code == requests.codes.ok:
+            logging.info('saved to wayback machine (status: %(status_code)s, URL: %(Content-Location)s, cache: %(X-Page-Cache)s)', res.headers)  # noqa
         else:
-            logging.warn('wayback machine failed (status: %(status_code)s, URL: %(Content-Location)s)',
-                         res.headers))
+            logging.warn('wayback machine failed (status: %(status_code)s, URL: %(Content-Location)s)', res.headers)  # noqa
