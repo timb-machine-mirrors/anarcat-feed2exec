34890d761c5fb4a90d3db6fb6c3c0063a78ec9f7
Antoine Beaupré <anarcat@debian.org>
2017-11-05 13:46:41 -0500

    sample betamax fixup configuration

--- feed2exec/tests/conftest.py
+++ feed2exec/tests/conftest.py
@@ -2,3 +2,4 @@ import betamax
 
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = 'feed2exec/tests/cassettes/'
+    # config.default_cassette_options['record_mode'] = 'new_episodes'
