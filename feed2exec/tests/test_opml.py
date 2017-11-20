bc7cba836ca1b858a1b01a82c401352adf3bbd78
Antoine Beaupré <anarcat@debian.org>
2017-11-20 09:54:28 -0500

    accept OPML test files without resulting .ini file

--- feed2exec/tests/test_opml.py
+++ feed2exec/tests/test_opml.py
@@ -12,9 +12,13 @@ testdir = utils.find_test_file()
 
 @pytest.mark.parametrize("opmlpath", glob(os.path.join(testdir, '*.opml')))  # noqa
 def test_import(test_db, conf_path, opmlpath):
-    with open(utils.find_test_file(opmlpath), 'rb') as opmlfile, \
-         open(utils.find_test_file(opmlpath[:-4] + 'ini')) as expected:
+    with open(utils.find_test_file(opmlpath), 'rb') as opmlfile:
         FeedManager().opml_import(opmlfile)
         assert conf_path.check()
-        assert expected.read() == conf_path.read()
+        try:
+            with open(utils.find_test_file(opmlpath[:-4] + 'ini')) as expected:
+                assert expected.read() == conf_path.read()
+        except FileNotFoundError:
+            # output is not considered if ini file is missing, just test parse
+            pass
     conf_path.remove()
