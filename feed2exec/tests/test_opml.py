1133af9fc535340a82f1e6057d48498e5b601aec
Antoine Beaupré <anarcat@debian.org>
2017-11-20 09:46:58 -0500

    use pytest parametrization for opml test files
    this allows us to cleanly separate test failures and simplifies code

--- feed2exec/tests/test_opml.py
+++ feed2exec/tests/test_opml.py
@@ -1,19 +1,20 @@
 from glob import glob
 import os.path
 
+import pytest
+
 from feed2exec.feeds import (FeedManager)
 from feed2exec.tests.fixtures import (test_db, conf_path)  # noqa
 import feed2exec.utils as utils
 
-def test_import(test_db, conf_path):  # noqa
-    testdir = utils.find_test_file('.')
-    print("looking in testdir %s" % testdir)
-    found = False
-    for opmlpath in glob(os.path.join(testdir, '*.opml')):
-        found = True
-        with open(utils.find_test_file(opmlpath), 'rb') as opmlfile, open(utils.find_test_file(opmlpath[:-4] + 'ini')) as expected:  # noqa
-            FeedManager().opml_import(opmlfile)
-            assert conf_path.check()
-            assert expected.read() == conf_path.read()
-        conf_path.remove()
-    assert found
+testdir = utils.find_test_file('.')
+
+
+@pytest.mark.parametrize("opmlpath", glob(os.path.join(testdir, '*.opml')))  # noqa
+def test_import(test_db, conf_path, opmlpath):
+    with open(utils.find_test_file(opmlpath), 'rb') as opmlfile, \
+         open(utils.find_test_file(opmlpath[:-4] + 'ini')) as expected:
+        FeedManager().opml_import(opmlfile)
+        assert conf_path.check()
+        assert expected.read() == conf_path.read()
+    conf_path.remove()
