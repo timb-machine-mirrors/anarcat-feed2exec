c44686a0665b3357905b23dad70063683a8a0f90
Antoine Beaupré <anarcat@debian.org>
2019-12-09 15:56:13 -0500

    update supported Python release numbers

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ classifiers = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Communications :: Email',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
     'Topic :: Text Processing :: Markup :: HTML',
