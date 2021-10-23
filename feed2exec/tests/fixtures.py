timb-machine-mirrors/anarcat-feed2exec

commit 0deb1efab43f30f213e6f5f728a422e52a1cd1c2

Add support for saving page resources to the Wayback Machine too
The SPN2 API used by this is not well documented and does
not return proper status codes or easily usable error text,
so make it enabled by default but add an option to disable it
and disable it in the tests, which need the right return codes.

The SPN1 API does return more specific error text in HTML in
the same way as SPN2 in some cases, so process that too.

See-also: https://blog.archive.org/2019/10/23/the-wayback-machines-save-page-now-is-new-and-improved/

feed2exec/plugins/wayback.py
@@ -2,8 +2,70 @@ import logging
 
 import requests
 
+import html5lib
 
 WAYBACK_URL = 'https://web.archive.org'
+
+ERROR_XPATHS = [
+    # XPath query for detecting blocking issues such as
+    # URL syntax errors or unresolvable domains.
+    #
+    # <h2>Sorry</h2>
+    # <p>Error text</p>
+    # ...
+    '//*[text()="Sorry"]/following-sibling::*/text()',
+
+    # XPath query for detecting errors of the saving process,
+    # the main one appears to be resaving the page too quickly.
+    #
+    # <h2 id="spn-title">Saving page https://example.org/</h2>
+    # <p>Error text</p>
+    # <div id="spn-option-mywebarchive">
+    # ...
+    '//*[starts-with(text(),"Saving page ")]' \
+    '/following-sibling::*[not(@id)]/text()',
+]
+
+
+def _check_response(url, res):
+    """Check the wayback machine response for errors
+
+    url is the URL that has been archived
+    res is the response from the archive request
+    """
+
+    # SPN2 POST requests always return HTML error pages that need parsing
+    # SPN1 GET requests return HTML error pages with HTTP error codes
+    parse = res.request.method == 'POST' or \
+            res.status_code != requests.codes.ok
+
+    if parse and res.text:
+        html = html5lib.parse(res.text, treebuilder="lxml")
+
+        for XPATH in ERROR_XPATHS:
+            error = html.xpath(XPATH)
+            if error:
+                break
+
+        if error and isinstance(error, list):
+            error = error[0]
+
+    else:
+        error = []
+
+    if error and res.status_code != requests.codes.ok:
+        logging.warning('wayback machine failed to save URL %s, status %d %s: %s',  # noqa
+                        url, res.status_code, res.reason, error)
+    elif not error and res.status_code != requests.codes.ok:
+        logging.warning('wayback machine failed to save URL %s, status %d %s',  # noqa
+                        url, res.status_code, res.reason)
+    elif error and res.status_code == requests.codes.ok:
+        logging.warning('wayback machine failed to save URL %s: %s',
+                        url, error)
+    elif not error and res.status_code == requests.codes.ok:
+        return True
+
+    return False
 
 
 def output(*args, feed=None, item=None, session=None, **kwargs):
@@ -11,36 +73,73 @@ def output(*args, feed=None, item=None, session=None, **kwargs):
     machine. It will retry URLs that fail, so it may be necessary to
     manually catchup feeds if they have broken `link` fields.
 
+    There are two wayback machine APIs that can be used, the default one
+    archives the full page while the other one archives only the page URLs.
+
+    The mechanism for archiving the full page uses a browser on the server
+    to download all the resources used by the page including img/CSS/JS/etc:
+
+    https://blog.archive.org/2019/10/23/the-wayback-machines-save-page-now-is-new-and-improved/
+
+    Unfortunately the SPN2 page is just a HTML form, the response code is
+    always 200 OK, any errors are returned in a HTML page and there are
+    no easily machine-readable ways to find the errors on the page so we
+    have to parse the HTML and query it using XPath based heuristics,
+    but if there are errors in a form that is not yet known then the
+    unknown errors will not be detected properly.
+
     Example::
 
       [NASA IOTD wayback]
       url = https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss
       output = feed2exec.plugins.wayback
+      args = full
 
     The above will save the Image of the day updates to the wayback
-    machine.
+    machine. Since the page has images and everything is loaded by JS,
+    using the SPN2 API is nessecary to capture the useful information.
+
+    Example::
+
+      [ikiwiki RecentChanges wayback]
+      url = https://ikiwiki.info/recentchanges/index.rss
+      output = feed2exec.plugins.wayback
+      args = page
+
+    The above will save the ikiwiki RecentChanges to the wayback machine.
+    Since the page is plain text, saving the full page resources is not
+    really nessecary, the CSS does not add information to the page.
     """
 
-    if item and item.get('link'):
-        wayback_url = '%s/save/%s' % (WAYBACK_URL, item.get('link'))
-        logging.debug('saving URL %s to wayback machine %s',
-                      item.get('link'), wayback_url)
+    full = not args or 'full' in args
+
+    if item:
+        url = item.get('link')
+
+    if item and url:
         if feed.get('catchup'):
             return True
-        res = session.get(wayback_url, allow_redirects=True)
-        res.headers['status_code'] = res.status_code
-        if res.history:
-            res.headers['Content-Location'] = res.history[0].headers.get('Content-Location')  # noqa
-        location = res.headers.get('Content-Location')
-        if location:
-            archive_location = WAYBACK_URL + location
-        else:
-            archive_location = 'N/A'
-        if res.status_code == requests.codes.ok:
-            logging.info('URL %s saved to wayback machine: %s',
-                         item.get('link'), archive_location)
+
+    # Use a browser on the server to download the entire page:
+    if item and url and full:
+        logging.debug('saving URL %s with resources to wayback machine', url)
+        wayback_url = '%s/save' % (WAYBACK_URL)
+        form = {
+            'url': url,
+            'capture_all': 'on', # Enables saving HTTP error pages
+        }
+        res = session.post(wayback_url, data=form)
+        if _check_response(url, res):
+            logging.info('URL %s with resources probably saved to wayback machine', url)  # noqa
             return True
-        else:
-            logging.warning('wayback machine failed to save URL %s, status %d',
-                            item.get('link'), res.status_code)  # noqa
-            return False
+
+    # Just save the URL and not any resources used by it
+    if item and url and not full:
+        logging.debug('saving URL %s without resources to wayback machine', url)  # noqa
+        wayback_url = '%s/save/%s' % (WAYBACK_URL, url)
+        res = session.get(wayback_url, allow_redirects=True)
+        if _check_response(url, res):
+            logging.info('URL %s saved without resources to wayback machine: %s', url, res.url)  # noqa
+            return True
+
+    return False

feed2exec/tests/fixtures.py
@@ -1,18 +1,37 @@
 import pytest
+import logging
+import logging.handlers
 
-from feed2exec.controller import FeedManager
+from feed2exec.controller import FeedManager, Feed
 import feed2exec.plugins.maildir
 
 
 @pytest.fixture()
-@pytest.mark.usefixtures('betamax_session')
-def feed_manager(tmpdir_factory, betamax_session):
+@pytest.mark.usefixtures('betamax_parametrized_recorder')
+def feed_manager_recorder(tmpdir_factory, betamax_parametrized_recorder):
     conf_path = tmpdir_factory.mktemp('config').join('feed2exex.ini')
     db_path = tmpdir_factory.mktemp('db').join('feed2exec.db')
-    return FeedManager(str(conf_path), str(db_path), session=betamax_session)
+    return (betamax_parametrized_recorder, FeedManager(str(conf_path), str(db_path), session=betamax_parametrized_recorder.session))
+
+
+@pytest.fixture()
+@pytest.mark.usefixtures('betamax_parametrized_session')
+def feed_manager(tmpdir_factory, betamax_parametrized_session):
+    conf_path = tmpdir_factory.mktemp('config').join('feed2exex.ini')
+    db_path = tmpdir_factory.mktemp('db').join('feed2exec.db')
+    return FeedManager(str(conf_path), str(db_path), session=betamax_parametrized_session)
 
 
 @pytest.fixture(autouse=True)
 def static_boundary(monkeypatch):
     monkeypatch.setattr(feed2exec.email, 'boundary',
                         '===============testboundary==')
+
+
+@pytest.fixture()
+def logging_handler():
+    handler = logging.handlers.MemoryHandler(0)
+    handler.setLevel('INFO')
+    logging.getLogger('').addHandler(handler)
+    logging.getLogger('').setLevel('DEBUG')
+    return handler

feed2exec/tests/test_plugins.py
@@ -4,8 +4,6 @@ from __future__ import print_function
 from glob import glob
 import datetime
 import email
-import logging
-import logging.handlers
 try:  # pragma nocover
     import unittest.mock as mock
 except ImportError:  # pragma nocover
@@ -16,6 +14,7 @@ import os.path
 from pkg_resources import parse_version
 import re
 import subprocess
+from time import sleep
 
 import feedparser
 import html2text
@@ -30,7 +29,12 @@ import feed2exec.plugins.maildir as maildir_plugin
 import feed2exec.plugins.transmission as transmission_plugin
 import feed2exec.plugins.archive as archive_plugin
 from feed2exec.tests.test_feeds import test_sample, test_params
-from feed2exec.tests.fixtures import (feed_manager, static_boundary)  # noqa
+from feed2exec.tests.fixtures import (
+    feed_manager,
+    feed_manager_recorder,
+    static_boundary,
+    logging_handler,
+)
 
 
 def test_maildir(tmpdir, feed_manager, static_boundary):  # noqa
@@ -194,38 +198,171 @@ def test_filter():
     assert p.called is not None
 
 
-def test_wayback(capfd, feed_manager):  # noqa
-    handler = logging.handlers.MemoryHandler(0)
-    handler.setLevel('INFO')
-    logging.getLogger('').addHandler(handler)
-    logging.getLogger('').setLevel('DEBUG')
-    feed = Feed('wayback test', {'output': 'feed2exec.plugins.wayback'})
-    item = feedparser.FeedParserDict({'link': 'http://example.com/'})
-    e = plugins.output(feed=feed, item=item, session=feed_manager.session)
-    assert e
-    for record in handler.buffer:
-        if 'wayback machine' in record.getMessage():
-            break
-    else:  # sanity check
-        raise AttributeError('no wayback logs generated?')  # pragma: nocover
-    assert 'INFO' == record.levelname
-    assert 'URL %s saved to wayback machine: %s' == record.msg
-    handler.buffer = []
-    item = feedparser.FeedParserDict({'link': 'http://example.com/404'})
-    e = plugins.output(feed=feed, item=item, session=feed_manager.session)
+@pytest.fixture(params=['page', 'full'])
+def wayback_feed(request):
+    config = {'output': 'feed2exec.plugins.wayback', 'args': request.param}
+    return Feed('wayback test', config)
+
+
+def test_wayback_archive(feed_manager, logging_handler, wayback_feed):
+    full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
+    item = feedparser.FeedParserDict({'link': 'http://archive.org/'})
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
     assert not e
-    for record in handler.buffer:
+    for record in logging_handler.buffer:
         if 'wayback machine' in record.getMessage():
             break
     else:  # sanity check
         raise AttributeError('no wayback logs generated?')  # pragma: nocover
     assert 'WARNING' == record.levelname
-    assert 'wayback machine failed to save URL %s, status %d' == record.msg
-    handler.buffer = []
+    if full:
+        assert 'wayback machine failed to save URL %s: %s' == record.msg
+        error = record.args[1]
+    else:
+        assert 'wayback machine failed to save URL %s, status %d %s: %s' == record.msg
+        assert 523 == record.args[1]
+        error = record.args[3]
+    assert 'Cannot save Internet Archive URLs! http://archive.org/.' == error
 
-    item = feedparser.FeedParserDict({'link':
-                                      'https://anarc.at/wikiicons/email.png'})
-    e = plugins.output(feed=feed, item=item, session=feed_manager.session)
+
+def test_wayback_invalid_example(feed_manager, logging_handler, wayback_feed):
+    full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
+    item = feedparser.FeedParserDict({'link': 'http://invalid.example.com/'})
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
+    assert not e
+    for record in logging_handler.buffer:
+        if 'wayback machine' in record.getMessage():
+            break
+    else:  # sanity check
+        raise AttributeError('no wayback logs generated?')  # pragma: nocover
+    assert 'WARNING' == record.levelname
+    if full:
+        assert 'wayback machine failed to save URL %s: %s' == record.msg
+        error = record.args[1]
+    else:
+        assert 'wayback machine failed to save URL %s, status %d %s: %s' == record.msg
+        assert 523 == record.args[1]
+        error = record.args[3]
+    assert 'Cannot resolve host invalid.example.com.' == error
+
+
+def test_wayback_example_invalid(feed_manager, logging_handler, wayback_feed):
+    full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
+    item = feedparser.FeedParserDict({'link': 'http://example.invalid/'})
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
+    assert not e
+    for record in logging_handler.buffer:
+        if 'wayback machine' in record.getMessage():
+            break
+    else:  # sanity check
+        raise AttributeError('no wayback logs generated?')  # pragma: nocover
+    assert 'WARNING' == record.levelname
+    if full:
+        assert 'wayback machine failed to save URL %s: %s' == record.msg
+        error = record.args[1]
+    else:
+        assert 'wayback machine failed to save URL %s, status %d %s: %s' == record.msg
+        assert 523 == record.args[1]
+        error = record.args[3]
+    assert 'http://example.invalid/ URL syntax is not valid.' == error
+
+
+def test_wayback_example_working(feed_manager, logging_handler, wayback_feed):
+    full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
+    if full:
+        link = 'http://example.com/'
+    else:
+        link = 'http://www.example.com/'
+    item = feedparser.FeedParserDict({'link': link})
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
+    assert e
+    for record in logging_handler.buffer:
+        if 'wayback machine' in record.getMessage():
+            break
+    else:  # sanity check
+        raise AttributeError('no wayback logs generated?')  # pragma: nocover
+    assert 'INFO' == record.levelname
+    if full:
+        assert 'URL %s with resources probably saved to wayback machine' == record.msg
+    else:
+        assert 'URL %s saved without resources to wayback machine: %s' == record.msg
+        assert record.args[1].startswith('https://web.archive.org/web/')
+
+
+def test_wayback_example_too_fast(feed_manager_recorder, logging_handler, wayback_feed):
+    recorder, feed_manager = feed_manager_recorder
+    # SPN1 does not error when archiving the same domain too fast
+    # so this test is only applicable to the SPN2 API.
+    full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
+    if not full: pytest.skip()
+    # Use example.org so we do not get false failures
+    # due to other tests, which all use example.com
+    item = feedparser.FeedParserDict({'link': 'http://example.org/'})
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
+    assert e
+    for record in logging_handler.buffer:
+        if 'wayback machine' in record.getMessage():
+            break
+    else:  # sanity check
+        raise AttributeError('no wayback logs generated?')  # pragma: nocover
+    assert 'INFO' == record.levelname
+    assert 'URL %s with resources probably saved to wayback machine' == record.msg
+
+    # Wait for the wayback machine to register the first request for the URL
+    # otherwise the second archiving request will succeed instead of failing.
+    # Don't bother waiting when we aren't really interacting with archive.org.
+    if recorder.current_cassette.is_recording():
+        sleep(30)
+
+    # Drop the messages from the first archiving request
+    logging_handler.buffer.clear()
+
+    # Now test that second archiving of same URL fails with "too fast" msg
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
+    assert not e
+    for record in logging_handler.buffer:
+        if 'wayback machine' in record.getMessage():
+            break
+    else:  # sanity check
+        raise AttributeError('no wayback logs generated?')  # pragma: nocover
+    assert 'WARNING' == record.levelname
+    assert 'wayback machine failed to save URL %s: %s' == record.msg
+    assert re.match(r'The same snapshot had been made .* ago\. We only allow new captures of the same URL every \d+ minutes\.', record.args[1])
+
+
+def test_wayback_example_404(feed_manager, logging_handler, wayback_feed):
+    # SPN2 does not return errors for HTTP error codes
+    # even if the error page archiving is turned off.
+    full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
+    if full: pytest.skip()
+    item = feedparser.FeedParserDict({'link': 'http://example.com/404'})
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
+    assert not e
+    for record in logging_handler.buffer:
+        if 'wayback machine' in record.getMessage():
+            break
+    else:  # sanity check
+        raise AttributeError('no wayback logs generated?')  # pragma: nocover
+    assert 'WARNING' == record.levelname
+    assert 'wayback machine failed to save URL %s, status %d %s: %s' == record.msg
+    assert 523 == record.args[1]
+    assert 'The server cannot find the requested resource http://example.com/404 (HTTP status=404).' == record.args[3]
+
+
+def test_wayback_catchup(feed_manager, logging_handler, wayback_feed):
+    full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
+    if full:
+        links = [
+            'https://anarc.at/wikiicons/diff.png',
+            'http://example.net/',
+        ]
+    else:
+        links = [
+            'https://anarc.at/wikiicons/email.png',
+            'http://www.example.net/',
+        ]
+    item = feedparser.FeedParserDict({'link': links[0]})
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
     assert e
 
     called = False
@@ -236,12 +373,13 @@ def test_wayback(capfd, feed_manager):  # noqa
         return mock.MagicMock()
 
     feed_manager.session.get = fake
-    item = feedparser.FeedParserDict({'link': 'http://example.com/'})
-    e = plugins.output(feed=feed, item=item, session=feed_manager.session)
+    feed_manager.session.post = fake
+    item = feedparser.FeedParserDict({'link': links[1]})
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
     assert called
-    feed['catchup'] = True
+    wayback_feed['catchup'] = True
     called = False
-    e = plugins.output(feed=feed, item=item, session=feed_manager.session)
+    e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
     assert not called
 
 

debian/control
@@ -12,11 +12,12 @@ Build-Depends: debhelper-compat (= 12)
              , python3-sphinx-rtd-theme
              , python3-pytest-cov
              , python3-pytest-runner
-             , python3-betamax
+             , python3-betamax (>= 0.8.0)
              , python3-click
              , python3-dateparser
              , python3-feedparser
              , python3-html2text
+             , python3-html5lib
              , python3-requests
              , python3-requests-file
              , python3-unidecode

setup.py
@@ -66,6 +66,7 @@ requires = [
     "requests",
     "requests-file",
     "unidecode",
+    "html5lib",
 ]
 
 packages = [
@@ -112,7 +113,7 @@ def sphinx2rst(path):
 
 
 test_requires = [
-    "betamax",
+    "betamax >= 0.8.0",
     "pyflakes",
     "pytest",
     "pytest-cov",

commit 0a58133f8b17c776f0cb95802288ac51fefd2f0b

Fix flake8 complaints introduced by Wayback plugin changes
Fixes: commit 0deb1efab43f30f213e6f5f728a422e52a1cd1c2

feed2exec/plugins/wayback.py
@@ -36,14 +36,13 @@ def _check_response(url, res):
 
     # SPN2 POST requests always return HTML error pages that need parsing
     # SPN1 GET requests return HTML error pages with HTTP error codes
-    parse = res.request.method == 'POST' or \
-            res.status_code != requests.codes.ok
+    parse = res.request.method == 'POST' or res.status_code != requests.codes.ok
 
     if parse and res.text:
         html = html5lib.parse(res.text, treebuilder="lxml")
 
-        for XPATH in ERROR_XPATHS:
-            error = html.xpath(XPATH)
+        for error_xpath in ERROR_XPATHS:
+            error = html.xpath(error_xpath)
             if error:
                 break
 
@@ -126,7 +125,7 @@ def output(*args, feed=None, item=None, session=None, **kwargs):
         wayback_url = '%s/save' % (WAYBACK_URL)
         form = {
             'url': url,
-            'capture_all': 'on', # Enables saving HTTP error pages
+            'capture_all': 'on',  # Enables saving HTTP error pages
         }
         res = session.post(wayback_url, data=form)
         if _check_response(url, res):

feed2exec/tests/fixtures.py
@@ -1,8 +1,8 @@
-import pytest
 import logging
 import logging.handlers
+import pytest
 
-from feed2exec.controller import FeedManager, Feed
+from feed2exec.controller import FeedManager
 import feed2exec.plugins.maildir
 
 
@@ -11,7 +11,14 @@ import feed2exec.plugins.maildir
 def feed_manager_recorder(tmpdir_factory, betamax_parametrized_recorder):
     conf_path = tmpdir_factory.mktemp('config').join('feed2exex.ini')
     db_path = tmpdir_factory.mktemp('db').join('feed2exec.db')
-    return (betamax_parametrized_recorder, FeedManager(str(conf_path), str(db_path), session=betamax_parametrized_recorder.session))
+    return (
+            betamax_parametrized_recorder,
+            FeedManager(
+                        str(conf_path),
+                        str(db_path),
+                        session=betamax_parametrized_recorder.session
+            )
+    )
 
 
 @pytest.fixture()

feed2exec/tests/test_plugins.py
@@ -29,12 +29,7 @@ import feed2exec.plugins.maildir as maildir_plugin
 import feed2exec.plugins.transmission as transmission_plugin
 import feed2exec.plugins.archive as archive_plugin
 from feed2exec.tests.test_feeds import test_sample, test_params
-from feed2exec.tests.fixtures import (
-    feed_manager,
-    feed_manager_recorder,
-    static_boundary,
-    logging_handler,
-)
+from feed2exec.tests.fixtures import (feed_manager, feed_manager_recorder, static_boundary, logging_handler)  # noqa
 
 
 def test_maildir(tmpdir, feed_manager, static_boundary):  # noqa
@@ -204,7 +199,7 @@ def wayback_feed(request):
     return Feed('wayback test', config)
 
 
-def test_wayback_archive(feed_manager, logging_handler, wayback_feed):
+def test_wayback_archive(feed_manager, logging_handler, wayback_feed):  # noqa
     full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
     item = feedparser.FeedParserDict({'link': 'http://archive.org/'})
     e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
@@ -225,7 +220,7 @@ def test_wayback_archive(feed_manager, logging_handler, wayback_feed):
     assert 'Cannot save Internet Archive URLs! http://archive.org/.' == error
 
 
-def test_wayback_invalid_example(feed_manager, logging_handler, wayback_feed):
+def test_wayback_invalid_example(feed_manager, logging_handler, wayback_feed):  # noqa
     full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
     item = feedparser.FeedParserDict({'link': 'http://invalid.example.com/'})
     e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
@@ -246,7 +241,7 @@ def test_wayback_invalid_example(feed_manager, logging_handler, wayback_feed):
     assert 'Cannot resolve host invalid.example.com.' == error
 
 
-def test_wayback_example_invalid(feed_manager, logging_handler, wayback_feed):
+def test_wayback_example_invalid(feed_manager, logging_handler, wayback_feed):  # noqa
     full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
     item = feedparser.FeedParserDict({'link': 'http://example.invalid/'})
     e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
@@ -267,7 +262,7 @@ def test_wayback_example_invalid(feed_manager, logging_handler, wayback_feed):
     assert 'http://example.invalid/ URL syntax is not valid.' == error
 
 
-def test_wayback_example_working(feed_manager, logging_handler, wayback_feed):
+def test_wayback_example_working(feed_manager, logging_handler, wayback_feed):  # noqa
     full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
     if full:
         link = 'http://example.com/'
@@ -289,12 +284,13 @@ def test_wayback_example_working(feed_manager, logging_handler, wayback_feed):
         assert record.args[1].startswith('https://web.archive.org/web/')
 
 
-def test_wayback_example_too_fast(feed_manager_recorder, logging_handler, wayback_feed):
-    recorder, feed_manager = feed_manager_recorder
+def test_wayback_example_too_fast(feed_manager_recorder, logging_handler, wayback_feed):  # noqa
+    recorder, feed_manager = feed_manager_recorder  # noqa
     # SPN1 does not error when archiving the same domain too fast
     # so this test is only applicable to the SPN2 API.
     full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
-    if not full: pytest.skip()
+    if not full:
+        pytest.skip()
     # Use example.org so we do not get false failures
     # due to other tests, which all use example.com
     item = feedparser.FeedParserDict({'link': 'http://example.org/'})
@@ -327,14 +323,18 @@ def test_wayback_example_too_fast(feed_manager_recorder, logging_handler, waybac
         raise AttributeError('no wayback logs generated?')  # pragma: nocover
     assert 'WARNING' == record.levelname
     assert 'wayback machine failed to save URL %s: %s' == record.msg
-    assert re.match(r'The same snapshot had been made .* ago\. We only allow new captures of the same URL every \d+ minutes\.', record.args[1])
+    assert re.match(
+        r'The same snapshot had been made .* ago\. We only allow new captures of the same URL every \d+ minutes\.',
+        record.args[1]
+    )
 
 
-def test_wayback_example_404(feed_manager, logging_handler, wayback_feed):
+def test_wayback_example_404(feed_manager, logging_handler, wayback_feed):  # noqa
     # SPN2 does not return errors for HTTP error codes
     # even if the error page archiving is turned off.
     full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
-    if full: pytest.skip()
+    if full:
+        pytest.skip()
     item = feedparser.FeedParserDict({'link': 'http://example.com/404'})
     e = plugins.output(feed=wayback_feed, item=item, session=feed_manager.session)
     assert not e
@@ -349,7 +349,7 @@ def test_wayback_example_404(feed_manager, logging_handler, wayback_feed):
     assert 'The server cannot find the requested resource http://example.com/404 (HTTP status=404).' == record.args[3]
 
 
-def test_wayback_catchup(feed_manager, logging_handler, wayback_feed):
+def test_wayback_catchup(feed_manager, logging_handler, wayback_feed):  # noqa
     full = 'args' not in wayback_feed or 'full' in wayback_feed['args']
     if full:
         links = [